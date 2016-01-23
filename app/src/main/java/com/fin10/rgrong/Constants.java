6c5d21d67d4fca15b7b6d8d62ef76d177dc0904f
fin10 <fender64@gmail.com>
2016-01-23 19:38:31 +0900

    version up

--- app/src/main/java/com/fin10/rgrong/Constants.java
+++ app/src/main/java/com/fin10/rgrong/Constants.java
@@ -7,13 +7,13 @@ public final class Constants {
 
     public static final class Url {
 
-        private Url() {
-        }
-
         public static final String MAIN = "http://te31.com/m/main.php";
         public static final String MAIN_NEW = "http://te31.com/m/main_new.php";
         public static final String LOGIN = "http://te31.com/m/main_login.php";
         public static final String WRITE = "http://te31.com/m/write.php";
         public static final String VIEW = "http://te31.com/m/view.php";
+
+        private Url() {
+        }
     }
 }
